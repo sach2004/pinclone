78a35f03b9eaf5b4eaeefd639def186ca3f85e15
sach2004 <132289265+sach2004@users.noreply.github.com>
2024-03-14 23:16:08 +0530

    finalProjectVercel

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/userdataddset");
+mongoose.connect("mongodb+srv://sachin:sachin@cluster0.nlf26rw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
 const plm = require("passport-local-mongoose");
 
